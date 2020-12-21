6366a3c96c9c18dda749c4d523084089c92948e4
HongMuchan <muchang0824@gmail.com>
2020-12-21 22:42:05 +0900

    [ADD]Store,Providerの実装

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
+import App from "./components/App";
+import "./App.scss";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'//※全ての階層からstoreにアクセスできようProviderの実装
+import {reducer} from './reducer'
 
-ReactDom.render(<App />, document.getElementById("root"));
+
+//storeの作成
+const store =createStore(reducer);
+
+ReactDom.render(
+    //上で作成したstoreを渡す
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
